66a3a1ab306a5873e42b6409f51739a514cfdc52
Patrick O'Brien <patobrien3590@gmail.com>
2026-09-18 18:24:29 -0400

    Make resource owner an explicit input so plans don't depend on who runs them

--- keyvault.tf
+++ keyvault.tf
@@ -37,7 +37,7 @@ resource "azurerm_key_vault" "main" {
 resource "azurerm_role_assignment" "deployer_secrets_officer" {
   scope                = azurerm_key_vault.main.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
+  principal_id         = local.admin_object_id
 }
 
 # Role assignments take a while to propagate; writing the secret immediately
